1ccdee02a0791b92708c348a302c4e89961475e9
mohamadkhalili <mohamadekhalili100100@gmail.com>
2023-06-16 16:06:48 +0330

    fix

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['isAuthenticated'] = require('..\\middleware\\isAuthenticated.js')
-middleware['isAuthenticated'] = middleware['isAuthenticated'].default || middleware['isAuthenticated']
-
-export default middleware
